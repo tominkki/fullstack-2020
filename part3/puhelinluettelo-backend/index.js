0f797d902ee7c44c34e62bdcecefcfd4fa9eed4d
unknown <tominkkinen@gmail.com>
2020-09-22 18:47:57 +0300

    3.2

--- part3/puhelinluettelo-backend/index.js
+++ part3/puhelinluettelo-backend/index.js
@@ -5,6 +5,17 @@ const persons = require('./persons');
 app.use(express.json());
 
 
+app.get('/info', (req, res) => {
+    
+    let date = new Date();
+    let count = persons.length;
+
+    res.send(
+        `<p>Phonebook has info for ${count} people</p>
+        <p>${date}</p>`
+    );
+});
+
 app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
