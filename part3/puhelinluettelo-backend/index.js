21319e058c7598cabb14ecbec4ef37cc9f7e90d8
unknown <tominkkinen@gmail.com>
2020-09-22 22:34:01 +0300

    3.8

--- part3/puhelinluettelo-backend/index.js
+++ part3/puhelinluettelo-backend/index.js
@@ -3,7 +3,10 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('req', (req, res) => JSON.stringify(req.body));
+
+app.use(morgan(':method :url :response-time :req'));
 
 let persons = [
     {
