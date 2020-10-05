d53d98f1f796561b8a0c9c8e27ac497cbc406b96
tomi <tominkkinen@gmail.com>
2020-10-05 15:24:57 +0300

    4.10

--- part4/blogilista/tests/blog-api.test.js
+++ part4/blogilista/tests/blog-api.test.js
@@ -3,6 +3,7 @@ const app = require('../app');
 const mongoose = require('mongoose');
 
 const api = supertest(app);
+const initialBlogs = 2;
 
 describe('api tests', () => {
 
@@ -14,12 +15,33 @@ describe('api tests', () => {
 
   test('there are 2 blogs in db', async() => {
     const res = await api.get('/api/blogs');
-    expect(res.body).toHaveLength(2);
+    expect(res.body).toHaveLength(initialBlogs);
   });
 
   test('returned objects have "id" field', async() => {
     const res = await api.get('/api/blogs');
     expect(res.body[0].id).toBeDefined();
+  });
+
+  test('blogs can be posted to db', async() => {
+    await api.post('/api/blogs')
+      .send(
+        {
+          title: "Jamix leipoo.",
+          author: "webmaster",
+          url: "safk.at",
+          likes: 1
+        }
+      )
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const res = await api.get('/api/blogs');
+    const authors = res.body.map(blogs => blogs.author);
+
+    expect(res.body).toHaveLength(initialBlogs+1);
+    expect(authors).toContain('webmaster');
+
   });
 
   afterAll(() => {
