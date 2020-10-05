864add9b146a336262598575ddf7c106ca342bf2
tomi <tominkkinen@gmail.com>
2020-10-05 10:00:37 +0300

    4.4

---
+++ part4/blogilista/tests/total-likes.test.js
@@ -0,0 +1,55 @@
+const { TestScheduler } = require('jest');
+
+const totalLikes = require('../utils/list_helper').totalLikes;
+
+describe('total likes', () => {
+  const blogs = [
+    {
+      title:"maistuis varmaan sullekki",
+      author:"Niilo22",
+      url:"google.com",
+      likes:222,
+      id:"5f79b923fc371a11b9f28ddf"
+    },
+    {
+      title:"oli ko hyvää",
+      author:"Pizzeria ja pub Teheran",
+      url:"google.com",
+      likes:69,
+      id:"5f79cea2fc371a11b9f28de0"
+    },
+    {
+      title: "Jamix leipoo.",
+      author: "webmaster",
+      url: "safk.at",
+      likes: 1,
+      id: "21093871249807"
+    }
+  ];
+
+  const oneBlog = [
+    {
+      title: "Jamix leipoo.",
+      author: "webmaster",
+      url: "safk.at",
+      likes: 1,
+      id: "21093871249807"
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = totalLikes([]);
+    expect(result).toBe(0);
+  });
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = totalLikes(oneBlog);
+    const likes = oneBlog[0].likes;
+    expect(result).toBe(likes);
+  });
+
+  test('of a bigger list is calculated right', () => {
+    const result = totalLikes(blogs);
+    expect(result).toBe(292)
+  });
+});
--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -1,5 +1,14 @@
-const dummy = (blogs) => (1);
+const dummy = blogs => (1);
+
+const totalLikes = blogs => {
+  const reducer = (sum, blog) => (
+    sum + blog.likes
+  );
+
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+}
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
