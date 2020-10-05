6b2f3ea378cc9038cc85b80a5faf3ac258a4b5ef
tomi <tominkkinen@gmail.com>
2020-10-05 14:44:37 +0300

    4.9

--- part4/blogilista/tests/blog-api.test.js
+++ part4/blogilista/tests/blog-api.test.js
@@ -4,17 +4,25 @@ const mongoose = require('mongoose');
 
 const api = supertest(app);
 
-test('blogs are returned as json', async() => {
-  await api.get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
+describe('api tests', () => {
 
-test('there are 2 blogs in db', async() => {
-  const res = await api.get('/api/blogs');
-  expect(res.body).toHaveLength(2);
-});
+  test('blogs are returned as json', async() => {
+    await api.get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-afterAll(() => {
-  mongoose.connection.close();
+  test('there are 2 blogs in db', async() => {
+    const res = await api.get('/api/blogs');
+    expect(res.body).toHaveLength(2);
+  });
+
+  test('returned objects have "id" field', async() => {
+    const res = await api.get('/api/blogs');
+    expect(res.body[0].id).toBeDefined();
+  });
+
+  afterAll(() => {
+    mongoose.connection.close();
+  });
 });
