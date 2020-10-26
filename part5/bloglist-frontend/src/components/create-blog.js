49fa2af55e08e897fc4e48a5849dd013310ac4bd
tomi <tominkkinen@gmail.com>
2020-10-26 10:45:24 +0200

    5.16

--- part5/bloglist-frontend/src/components/create-blog.js
+++ part5/bloglist-frontend/src/components/create-blog.js
@@ -31,21 +31,21 @@ const CreateBlog = ({ addBlog }) => {
             <tr>
               <td>title: </td>
               <td>
-                <input value = {title}
+                <input id = 'title' value = {title}
                   onChange = {({ target }) => setTitle(target.value)}/>
               </td>
             </tr>
             <tr>
               <td>author: </td>
               <td>
-                <input value = {author}
+                <input id = 'author' value = {author}
                   onChange = {({ target }) => setAuthor(target.value)}/>
               </td>
             </tr>
             <tr>
               <td>url: </td>
               <td>
-                <input value = {url}
+                <input id = 'url' value = {url}
                   onChange = {({ target }) => setUrl(target.value)}/>
               </td>
             </tr>
