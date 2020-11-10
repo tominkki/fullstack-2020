818d678c8465cabd0c19f0379b7cfd9868f5fbfe
tomi <tominkkinen@gmail.com>
2020-11-10 12:19:56 +0200

    8.5

--- part8/library/library-backend/index.js
+++ part8/library/library-backend/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -111,10 +111,15 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (parent, args) => (
-      !args.author ? books
-        : books.filter(b => b.author === args.author)
-        ),
+    allBooks: (parent, args) => {
+      if(args.author && args.genre) {
+        return books.filter(b => b.author === args.author && b.genres.includes(args.genre));
+      }
+      else if(args.author || args.genre) {
+        return books.filter(b => b.author === args.author || b.genres.includes(args.genre));
+      }
+      else return books;
+    },
     allAuthors: () => authors
   },
   Author: {
