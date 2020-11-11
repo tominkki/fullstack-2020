78c1fd62a0c943aef320e02cba37327a8d0fda1d
tomi <tominkkinen@gmail.com>
2020-11-11 18:38:58 +0200

    8.15

--- part8/library/library-backend/graphql/mutations.js
+++ part8/library/library-backend/graphql/mutations.js
@@ -1,15 +1,29 @@
+const { UserInputError } = require('apollo-server');
 const Book = require('../models/book');
 const Author = require('../models/author');
 
 const addBook = async (parent, args) => {
   const book = new Book({ ...args });
-  return await book.save();
+
+  try{
+    return await book.save();
+  } catch (e) {
+    throw new UserInputError(e.message, {
+      invalidArgs: args
+    });
+  }
 };
 
 const editAuthor = async (parent, args) => {
-  const author = await Author.findOne({ name: args.name });
-  author.born =  args.setBornTo;
-  return await author.save();
+  try{
+    const author = await Author.findOne({ name: args.name });
+    author.born =  args.setBornTo;
+    return await author.save();
+  } catch (e) {
+    throw new UserInputError(e.message, {
+      invalidArgs: args
+    });
+  }
 };
 
 module.exports = { addBook, editAuthor };
